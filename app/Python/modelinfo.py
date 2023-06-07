9c9311c6e8dcf70b261dc182f7391fa62a208b9c
Coralsea0017 <127711696+Coralsea0017@users.noreply.github.com>
2023-06-07 11:34:50 +0900

    Update modelinfo.py

--- app/Python/modelinfo.py
+++ app/Python/modelinfo.py
@@ -1,35 +1,56 @@
+import sys
 import numpy as np
 import cv2
 import tensorflow as tf
 
+# Read the image path from command-line arguments
+image_path = sys.argv[1]
+
 # Load the image
-image_path = "storage/app/iamges/image.jpg"
 image = cv2.imread(image_path)
 
-# Resize the image to 224x224
-image = cv2.resize(image, (224, 224))
+# Rest of the code remains the same...
 
-# Normalize the image
-image = image.astype('float32') / 255.0
+# Check if the image is successfully loaded
+if image is not None:
+    # Resize the image to 224x224
+    image = cv2.resize(image, (224, 224))
 
-# Load the TFLite model
-model_path = "app/Python/model/VGG16dec.tflite"
-interpreter = tf.lite.Interpreter(model_path=model_path)
-interpreter.allocate_tensors()
+    # Normalize the image
+    image = image.astype('float32') / 255.0
 
-# Get input and output details
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+    # Load the TFLite model
+    model_path = "/home/users/2/deca.jp-broad-hyuga-0115/web/fcdb/app/Python/model/TLVGG16.tflite"
+    interpreter = tf.lite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
 
-# Set input tensor
-input_data = np.expand_dims(image, axis=0)
-interpreter.set_tensor(input_details[0]['index'], input_data)
+    # Get input and output details
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
 
-# Perform inference
-interpreter.invoke()
+    # Set input tensor
+    input_data = np.expand_dims(image, axis=0)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
 
-# Get the output tensor
-output_data = interpreter.get_tensor(output_details[0]['index'])
+    # Perform inference
+    interpreter.invoke()
 
-# Print the predicted output
-print(output_data)
+    # Get the output tensor
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+
+    # Load class names
+    class_names = ['2194201', '2294201', '2294207']  # Replace with your own class names
+
+    # Find the index of the closest match
+    closest_index = np.argmax(output_data)
+
+    # Get the class name of the closest match
+    closest_class = class_names[closest_index]
+
+    # Store recognition probability in a separate table or variable
+    recognition_probability = output_data[0][closest_index]
+
+    # Print the predicted output
+    print(closest_class)
+else:
+    print("Failed to load the image:", image_path)
